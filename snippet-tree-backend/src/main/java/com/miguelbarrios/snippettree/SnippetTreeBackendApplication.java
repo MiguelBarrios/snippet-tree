87d5d9aa0e71046ad1464ae635e35e43037ecd87
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-02 15:33:18 -0500

    added gitignore, created spring boot project

---
+++ snippet-tree-backend/src/main/java/com/miguelbarrios/snippettree/SnippetTreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miguelbarrios.snippettree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetTreeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetTreeBackendApplication.class, args);
+	}
+
+}
---
+++ snippet-tree-backend/src/test/java/com/miguelbarrios/snippettree/SnippetTreeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelbarrios.snippettree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetTreeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
