e4aeb84585ed06002ed27fefd92686f6ba480a2a
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-06 14:37:07 -0500

    created snippet display component

--- snippet-tree-frontend/src/app/app.module.ts
+++ snippet-tree-frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { NgbDropdown, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SnippetDisplayComponent } from './shared/components/snippet-display/snippet-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SnippetDisplayComponent
   ],
   imports: [
     BrowserModule,
