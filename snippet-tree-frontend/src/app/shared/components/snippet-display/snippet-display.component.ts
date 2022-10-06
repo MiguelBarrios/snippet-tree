976c00791b58f8ab95c0394d16e16f5b186ff735
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-06 16:58:34 -0500

    finished method to load snippet

--- snippet-tree-frontend/src/app/shared/components/snippet-display/snippet-display.component.ts
+++ snippet-tree-frontend/src/app/shared/components/snippet-display/snippet-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Snippet } from '../../models/snippet';
 import { SnippetService } from '../../services/snippet.service';
 
 @Component({
@@ -9,17 +10,22 @@ import { SnippetService } from '../../services/snippet.service';
 })
 export class SnippetDisplayComponent implements OnInit {
 
+  activeSnippet: Snippet | null  = null;
+  
+
   constructor(private snippetService: SnippetService ) { }
 
   ngOnInit(): void {
     this.getSnippetById("633f4a9c60e5d9630f9494fe");
   }
 
+
   getSnippetById(snippetId: String){
     console.log(snippetId);
     this.snippetService.getSnippetById(snippetId).subscribe(
-      (data) => {
-        console.log(data);
+      (snippet) => {
+        console.log(snippet);
+        this.activeSnippet = snippet;
       },
       (error) => {
 
