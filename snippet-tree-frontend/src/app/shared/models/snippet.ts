a0d32958b48da09f2dc76fd8752bcf7e154e5ac6
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-06 16:06:03 -0500

    created snippet model

---
+++ snippet-tree-frontend/src/app/shared/models/snippet.ts
@@ -0,0 +1,10 @@
+export class Snippet {
+
+    id: string;
+    content: string;
+
+    constructor(  id: string = "", content: string = "") {
+        this.id = id,
+        this.content = content;
+    }
+}
--- snippet-tree-frontend/src/app/shared/services/snippet.service.ts
+++ snippet-tree-frontend/src/app/shared/services/snippet.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnippetService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/v1/snippets/'
+
+  constructor(private http:HttpClient) { }
+
+  getSnippetById(snippetId: String){
+    // return this.http.get<>
+
+  }
 }
