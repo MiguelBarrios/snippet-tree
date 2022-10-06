d62749c6eab3c032ac4c8aee7db0f83c739e8d52
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-06 15:01:02 -0500

    added routing

--- snippet-tree-frontend/src/app/app-routing.module.ts
+++ snippet-tree-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard/user-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user-dashboard', component: UserDashboardComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
