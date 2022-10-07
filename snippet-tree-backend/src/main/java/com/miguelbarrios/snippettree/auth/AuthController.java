3fdccc0c4f2de2ce6e9e356729f27210143e48b8
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-07 15:16:22 -0500

    created auth controller

---
+++ snippet-tree-backend/src/main/java/com/miguelbarrios/snippettree/auth/AuthController.java
@@ -0,0 +1,37 @@
+package com.miguelbarrios.snippettree.auth;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.miguelbarrios.snippettree.user.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin({"*","http://localhost"})
+public class AuthController {
+	
+	@Autowired
+	private AuthService authService;
+	
+	@PostMapping(path = "/register")
+	public User register(@RequestBody User user, HttpServletResponse res) {
+	    if (user == null) {
+	        res.setStatus(400);
+	    }
+	    user = authService.register(user);
+	    return user;
+	}
+
+	@GetMapping(path = "/authenticate")
+	public User authenticate(Principal principal) {
+	    return authService.getUserByUsername(principal.getName());
+	}
+
+}
